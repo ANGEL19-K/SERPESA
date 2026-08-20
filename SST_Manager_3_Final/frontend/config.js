53dd69a95835c3aab4ed9abd54c9ce81f6bd7355
ANGEL19-K <angelve581@gmail.com>
2026-08-20 17:31:17 -0500

    update

--- SST_Manager_3_Final/frontend/config.js
+++ SST_Manager_3_Final/frontend/config.js
@@ -1,5 +1,5 @@
 window.SST_CONFIG = {
   SUPABASE_URL: "https://bxrhjuayvyltygxfwfqj.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_Uez5cTomAgpd4tlee0wyXA_ZOA8kME4",
-  API_URL: "https://TU-SST-MANAGER-API.onrender.com"
+  API_URL: "https://sst-manager-api.onrender.com"
 };
